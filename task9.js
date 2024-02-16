90a1d0b7065fb05277f8c8dccd40534751f1b32b
MercyOlu <mercyolukoya@gmail.com>
2024-02-16 17:49:15 +0100

    Create task9.js
    Task 9

---
+++ task9.js
@@ -0,0 +1,6 @@
+function calculateAreaOfRectangle(length, width){
+
+    const product = length * width;
+       console.log('The area of the rectangle is '+product);
+         }
+    calculateAreaOfRectangle(10,5);
